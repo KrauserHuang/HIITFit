6f812672575254e28fc7af90be6d5a054a4feb25
Krauser Huang <crescentdence@gmail.com>
2024-10-26 13:16:54 +0800

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Tai Chin Huang on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
