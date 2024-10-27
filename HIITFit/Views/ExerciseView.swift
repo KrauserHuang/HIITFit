99caa1b57653cc569cd5526f9f07ca4203aeac08
Krauser Huang <crescentdence@gmail.com>
2024-10-27 15:16:55 +0800

    [Feature] Create ExerciseView/WelcomeView
    1. 新增Views資料夾，將ContentView/ExerciseView/WelcomeView移進去

---
+++ HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseView.swift
+//  HIITFit
+//
+//  Created by Tai Chin Huang on 2024/10/27.
+//
+
+import SwiftUI
+
+struct ExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseView()
+}
---
+++ HIITFit/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  HIITFit
+//
+//  Created by Tai Chin Huang on 2024/10/27.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
