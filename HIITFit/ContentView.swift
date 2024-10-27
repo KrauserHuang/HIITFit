3d079b7840a9922483df1b4c48da44847f3c8bf0
Krauser Huang <crescentdence@gmail.com>
2024-10-27 15:12:45 +0800

    [Feature] Change tabView style
    1. 修改tabView style（像page一樣可以左右滑動）
    2. 讓預設的tabView pageControl顯示於背景為白底畫面

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -11,12 +11,13 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Text("Welcome")
-                .tabItem { Text("Welcome") }
             Text("Exercise 1")
-                .tabItem { Text("Exercise 1") }
             Text("Exercise 2")
-                .tabItem { Text("Exercise 2") }
         }
+        // 將tabView變成像頁面滑動的特效（附有pageControl）
+        .tabViewStyle(PageTabViewStyle())
+        // 但因為pageControl預設為白色，故需要加上PageIndexViewStyle讓他顯示
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
     }
 }
 
