b985f793b85d24a3d45f26ef791639b4f8cd649f
Krauser Huang <crescentdence@gmail.com>
2024-10-27 15:04:19 +0800

    [Feature] Add tabItem
    增加底部tabItem文字

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -11,8 +11,11 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Text("Welcome")
+                .tabItem { Text("Welcome") }
             Text("Exercise 1")
+                .tabItem { Text("Exercise 1") }
             Text("Exercise 2")
+                .tabItem { Text("Exercise 2") }
         }
     }
 }
