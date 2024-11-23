54ce25d1c06156a74f1757738f873750d0d6b842
Krauser Huang <crescentdence@gmail.com>
2024-11-23 17:33:08 +0800

    [Feature] Add TimerView in ExerciseView
    1. Add @State showTimer/timerDone to control TimerView and doneButton
    2. startButton to trigger TimerView
    3. reset timerDone/showTimer on doneButton

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct ExerciseView: View {
     
     var startButton: some View {
-        Button("Start Exercise") {}
+        Button("Start Exercise") {
+            showTimer.toggle()
+        }
     }
     
     var doneButton: some View {
         Button("Done") {
+            // doneButton要能點擊時timerDone為true，所以在點擊doneButton則要重置timerDone為false(disable doneButton)
+            // showTimer當下狀態為true，所以要重置為false(隱藏TimerView)
+            timerDone = false
+            showTimer.toggle()
             if lastExercise {
                 showSuccess.toggle()
             } else {
@@ -27,6 +33,8 @@ struct ExerciseView: View {
     @State private var rating = 0
     @State private var showHistory = false
     @State private var showSuccess = false
+    @State private var showTimer = false
+    @State private var timerDone = false
     let timeInterval: TimeInterval = 30
     let index: Int
     var exercise: Exercise {
@@ -43,11 +51,13 @@ struct ExerciseView: View {
                     .padding(.bottom)
                 VideoPlayerView(name: exercise.videoName)
                     .frame(height: geometry.size.height * 0.45)
-                Text(Date().addingTimeInterval(timeInterval), style: .timer)
-                    .font(.system(size: geometry.size.height * 0.07))
+                if showTimer {
+                    TimerView(timerDone: $timerDone, size: geometry.size.height * 0.07)
+                }
                 HStack(spacing: 150) {
                     startButton
                     doneButton
+                        .disabled(!timerDone) // 利用timerDone來判斷是否該enable Done按鈕
                         .sheet(isPresented: $showSuccess) {
                             SuccessView(selectedTab: $selectedTab)
                                 .presentationDetents([.medium, .large])
